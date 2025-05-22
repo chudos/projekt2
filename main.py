39feaa31bc0f8d80680f42f16dc3d1373fc80615
AdamKrejci <adam.krejci.1915@gmail.com>
2025-05-22 13:13:57 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -24,61 +24,72 @@ def kontrola_hrace(hrac):
         return True
     else:
         return False
-    
-print(
-"Hi there! \n"
-"-----------------------------------------------\n"
-"I've generated a random 4 digit number for you.\n"
-"Let's play a bulls and cows game.\n"
-"-----------------------------------------------\n"
-"Enter a number:\n"
-"-----------------------------------------------"
-)
 
-spravne_cislo = generuj_spravne_cislo()
-pocet_pokusu = 0
-start = time.time()
-
-while True:
-    while True:
-        volba_hrace = input("Enter a 4-digit number with unique digits:")
-        if kontrola_hrace(volba_hrace):
-            break
-        else:
-            print ("Wrong input, try again")
-    pocet_pokusu += 1
+def vyhodnot_tip(tajny, tip):
     bulls = 0
-    for pocitac, hrac in zip(spravne_cislo, volba_hrace):
-        if pocitac == hrac:
+    for p, h in zip(tajny, tip):
+        if p == h:
             bulls += 1
     cows = 0
-    for hrac in volba_hrace:
-        if hrac in spravne_cislo:
+    for h in tip:
+        if h in tajny:
             cows += 1
     cows -= bulls
-    if bulls == 4:
-        end = time.time()
-        cas = end - start
-        print(
-        ">>>", volba_hrace, "\n"
-        "Correct, you've guessed the right number\n"
-        "in", pocet_pokusu, "guesses\n"
+    return bulls, cows
+
+def vypis_vysledek(tip, bulls, cows):
+    print(f">>> {tip}")
+    print(f"{bulls} bulls, {cows} cows")
+    print("-----------------------------------------------")
+
+def main():
+    print(
+        "Hi there! \n"
+        "-----------------------------------------------\n"
+        "I've generated a random 4 digit number for you.\n"
+        "Let's play a bulls and cows game.\n"
+        "-----------------------------------------------\n"
+        "Enter a number:\n"
         "-----------------------------------------------"
-        )
-        if cas > 60:
-            minuty = int(cas//60)
-            sekundy = round(cas % 60)
+    )
+
+    spravne_cislo = generuj_spravne_cislo()
+    pocet_pokusu = 0
+    start = time.time()
+
+    while True:
+        while True:
+            volba_hrace = input("Enter a 4-digit number with unique digits:")
+            if kontrola_hrace(volba_hrace):
+                break
+            else:
+                print("Wrong input, try again")
+        pocet_pokusu += 1
+        bulls, cows = vyhodnot_tip(spravne_cislo, volba_hrace)
+        if bulls == 4:
+            end = time.time()
+            cas = end - start
             print(
-                "You did it in", minuty, "minutes", sekundy, "seconds\n"
-                "That's not bad!"
+                ">>>", volba_hrace, "\n"
+                "Correct, you've guessed the right number\n"
+                "in", pocet_pokusu, "guesses\n"
+                "-----------------------------------------------"
             )
+            if cas > 60:
+                minuty = int(cas//60)
+                sekundy = round(cas % 60)
+                print(
+                    "You did it in", minuty, "minutes", sekundy, "seconds\n"
+                    "That's not bad!"
+                )
+            else:
+                print(
+                    "You did it in", round(cas), "seconds\n"
+                    "That's amazing!"
+                )
+            break
         else:
-            print(
-                "You did it in", round(cas), "seconds\n"
-                "That's amazing!"
-            )
-        break
-    else:
-        print( ">>>", volba_hrace)
-        print(bulls, "bulls", cows, "cows")
-        print("-----------------------------------------------")
+            vypis_vysledek(volba_hrace, bulls, cows)
+
+if __name__ == "__main__":
+    main()
